b762b690ca52f07bca541a508680c813203997be
prohashim <prohashim4e@gmail.com>
2023-10-21 21:22:41 +0500

    class 3 done

---
+++ class3/app.py
@@ -0,0 +1,23 @@
+# keywords - predefined words that can perform some actions
+
+# variables:
+# Take variable as a box where we can keep something (screw driver) inside of it and if we want to use that we can take the box and use the stuff that we need.
+
+
+# identifier (The name of the variable is called identifier)
+# Assignment operator (=)
+# Variable assignment
+greetings = "Hello, hi this is Ben and I am going to be your host for today :D"
+box = "I have screw driver inside of me"
+
+age = 56
+print(age)
+
+# reassigning variables 
+age = input("What is your age? ")
+
+print(greetings)
+print(box)
+# return value -> a value that we get after printing something or after a using a variable
+print("hello your age is: ", age)
+
