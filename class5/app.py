b6a7a5c5f523dd7485dffc5d17610bf44900b8a6
prohashim <prohashim4e@gmail.com>
2023-11-11 23:59:41 +0500

    class 5 done

---
+++ class5/app.py
@@ -0,0 +1,47 @@
+# data types: the type of data that we store in a variable
+
+# String
+# "" / ' / '''   '''
+greeings = "Hello hi wonderful day!"
+
+myage = '56545'
+
+mylongstr = '''
+
+I can write 
+multi 
+lines string
+'''
+
+# Number / integer
+myid = 34587364562
+
+
+# Float 
+temp = 56.7
+
+# Boolean 
+is_cloudy = True
+is_hot = False
+
+
+#What is concatenation?
+# Adding two strings / combining two strings is called contenation
+
+print("Helllooo" + " Ben" + " i am someone", "yoooo")
+
+print(34, 56)
+
+# Why concatenation?
+# to create new strings
+
+# Errors
+
+# Syntax - error in the syntax
+# ltr hg = 56
+
+# Type error
+
+hello = 45 + "HELLOOOO I AM HOME"
+
+hi = 'A'
