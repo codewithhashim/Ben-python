b9409893a792a605aa71916ad32903aab0b785a7
prohashim <prohashim4e@gmail.com>
2023-11-19 22:30:58 +0500

    class 6 done

---
+++ class6/app.py
@@ -0,0 +1,57 @@
+myage = 67
+your_age = 100
+
+print(myage + your_age)
+
+print(45 + 45)
+
+print(45 - 45)
+
+print(45 * 45)
+
+print(45 / 45)
+
+# modulus operator
+print(" The remainder is: ", 5 % 3)
+
+print (2 ** 3)
+
+
+# 4 + 7 
+# 4 and 7 are operands and + is an operator
+
+# operator 
+# A symbol that operates OR combines two values in an expression.
+# Arithmetic operator
+# +, -, *, /, %, **
+
+# operands
+# a + b 
+#  5 + 6
+
+# expression: a sequence of characters that can produce a value.
+# combination of values, variables and operators to produce a value.
+#  5 + 6 = 11
+
+# Binary operator 
+# that operates two different operands 
+# a + b = c
+
+# Modulus operator
+# an operator that finds the remainder of a division
+
+# incrementing
+print (your_age + 1)
+
+# decrementing
+print (your_age - 1)
+
+# float is a data type.
+# decimal point 
+temprature = 46.5
+
+print ( 2 + 2 *2 /4 - 3)
+# 2 / 4 ->> 0.5
+# 2 * .5 ->>>> 1
+# 2 + 1 ->>> 3
+# 3-3 ->>> 0
