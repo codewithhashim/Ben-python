fc2c9f8a662de9a9080b861b7186eceb05a23fbc
prohashim <prohashim4e@gmail.com>
2023-12-10 22:11:16 +0500

    class 9 concept 1

---
+++ class9/app.py
@@ -0,0 +1,31 @@
+# if statement
+# indentation
+# we can use indentation in cases:
+# if, loops, funcitons or classes
+
+# we need indentation to denote a code is inside a same block
+
+if ():
+    # print()
+    # print()
+    pass
+
+if ():
+    pass
+
+# Loop
+# A code that can run multiple times based on a condition
+
+# Iteration: A single repetition of a loop. We often talk about loops in terms of the number of iterations that it has.
+
+# Iterator Variable: The variable that stores a value which will determine the number of iterations.
+
+autho_name = "Mark Manson"
+number = 5
+
+# while loop
+# while is a keyword
+while number < 10:
+    # incrementing
+    print(autho_name)
+    number = number + 1
