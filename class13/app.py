40c892fafad34c031c9c6f2e79f5d53b6a9e7977
prohashim <prohashim4e@gmail.com>
2024-01-06 21:59:38 +0500

    class 13 list sort()

---
+++ class13/app.py
@@ -0,0 +1,14 @@
+# count method for list
+
+bens_numbers = [3, 4, 5, '3', '3', 6, 7, 8, '3', '3']
+print(bens_numbers.count('3'))
+
+# sort()
+bens_numbers_int = [3, 4, 5, 6, 7, 8, 2, 56, 7]
+# bens_numbers_int.sort()  # ascending order by default
+# bens_numbers_int.sort(reverse=True) -> descending order
+
+# print(bens_numbers_int)
+
+bens_numbers_int.reverse()
+print(bens_numbers_int)
