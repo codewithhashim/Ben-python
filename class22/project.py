7eda96254307f1204cc999e023f2f937332992d7
codewithhashim <codewithhashim@gmail.com>
2024-03-16 12:33:53 -0400

    class 22 is done

---
+++ class22/project.py
@@ -0,0 +1,22 @@
+receptionist = "Alex"
+
+print("Receptionist: Hi, what would you like to order? ")
+
+customerChoice = input("We have XYZ available in the menu: ")
+
+
+def cookPizza():
+    print("The pizza is in process and will be served shortly")
+
+if customerChoice == "pizza":
+    cookPizza()
+    print("3,2,1.... Pizza is ready!")
+
+
+customerRating = input("How would you like to rate your experience with Alex? ")
+
+# logic for the star / experience rating 
+
+
+
+
